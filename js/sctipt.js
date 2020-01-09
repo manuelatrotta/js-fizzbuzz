8f53ce35eb899d4eea649c307ed75d1af94596e9
manuelatrotta <manuelat88@hotmail.it>
2020-01-09 14:17:00 +0100

    stampa numeri da 1 a 100

--- js/sctipt.js
+++ js/sctipt.js
@@ -1,1 +1,7 @@
-alert('fizzquiz')
+alert('fizzquiz');
+//Scrivi un programma che stampi i numeri da 1 a 100,ma per i multipli di 3 stampi “Fizz” alposto del numero e per i multipli di 5 stampi Buzz.Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+
+//step 1: scrivere un programma che stampi i numeri da 1 a 100
+for(var i=100; i>0; i++){
+  console.log(i);
+}
