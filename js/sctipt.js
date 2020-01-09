1b1d167735b704561193f143d6566e10bda93cf7
manuelatrotta <manuelat88@hotmail.it>
2020-01-09 15:37:32 +0100

    riorganizzazione condizioni

--- js/sctipt.js
+++ js/sctipt.js
@@ -8,8 +8,8 @@ for(var i=100; i>=0; i--){
   document.getElementById('listaNumeri').innerHTML += '<li>'+ i+ '</li>';
   if(i % 5 == 0 && i % 3 == 0)
   console.log('fizzbuzz');
-  else if (i % 5 == 0)
-  console.log('Buzz');
   else if(i % 3 == 0)
   console.log('fizz');
+  else if (i % 5 == 0)
+  console.log('Buzz');
 }
