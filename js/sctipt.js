885418629548c8d72d0f9951b9518e3484f5a3db
manuelatrotta <manuelat88@hotmail.it>
2020-01-09 15:47:28 +0100

    {} aggiunte il secondo else if

--- js/sctipt.js
+++ js/sctipt.js
@@ -8,9 +8,9 @@ for(var i=0; i<=100; i++){
   document.getElementById('listaNumeri').innerHTML += '<li>'+ i+ '</li>';
   if(i % 5 == 0 && i % 3 == 0){
   console.log('fizzbuzz');
-  }else if(i % 3 == 0)
+  }else if(i % 3 == 0){
   console.log('fizz');
-  else if (i % 5 == 0){
+  }else if (i % 5 == 0){
   console.log('Buzz');
   }
 }
