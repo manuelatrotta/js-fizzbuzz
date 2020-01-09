d77dac6563ec398e8b11df035d2cd8fb3d0b66a6
manuelatrotta <manuelat88@hotmail.it>
2020-01-09 15:08:59 +0100

    multipli di 3 e 5 fizzbuzz

--- js/sctipt.js
+++ js/sctipt.js
@@ -2,10 +2,13 @@ alert('fizzquiz');
 //Scrivi un programma che stampi i numeri da 1 a 100,ma per i multipli di 3 stampi “Fizz” alposto del numero e per i multipli di 5 stampi Buzz.Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 //step 1: scrivere un programma che stampi i numeri da 1 a 100
 //step 2:individua multipli di 3 e stampa 'Fizz' al posto del numero
+//step 3:individua multipli di 3 e 5 ergo di 15 e stampa 'fizzbuzz'
 for(var i=100; i>=0; i--){
   console.log(i);
-  if(i % 3 ==0)
+  if(i % 3 == 0)
   console.log('fizz');
   else if (i % 5 == 0)
-  console.log("Buzz");
+  console.log('Buzz');
+  else if(i % 5 == 0 && i % 3 == 0)
+  console.log('fizzBuzz');
 }
